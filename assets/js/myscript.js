fb4a0ba69476ff92bd29381d3bd19661317ca6fe
Analisa1984 <ana_lisa8@hotmail.com>
2026-01-16 15:42:41 +0000

    Create new myscript.js file. Add variables for game array. Add randomGameArray Item formula.

---
+++ assets/js/myscript.js
@@ -0,0 +1,76 @@
+let userAnswer; //document.getElementById("user-answer").value;
+let score = 0;
+let buttons = document.getElementsByTagName("button");
+let round = 1;
+
+const zero = document.getElementById("zero").innerHTML;
+  const one = document.getElementById("one").innerHTML;
+  const two = document.getElementById("two").innerHTML;
+  const three = document.getElementById("three").innerHTML;
+  const four = document.getElementById("four").innerHTML;
+  const five = document.getElementById("five").innerHTML;
+  const six = document.getElementById("six").innerHTML;
+  const seven = document.getElementById("seven").innerHTML;
+  const eight = document.getElementById("eight").innerHTML;
+  const nine = document.getElementById("nine").innerHTML;
+
+  const a = document.getElementById("a").innerHTML;
+  const b = document.getElementById("b").innerHTML;
+  const c = document.getElementById("c").innerHTML;
+  const d = document.getElementById("d").innerHTML;
+  const e = document.getElementById("e").innerHTML;
+  const f = document.getElementById("f").innerHTML;
+  const g = document.getElementById("g").innerHTML;
+  const h = document.getElementById("h").innerHTML;
+  const i = document.getElementById("i").innerHTML;
+  const j = document.getElementById("j").innerHTML;
+  const star = document.getElementById("star").innerHTML;
+  const heart = document.getElementById("heart").innerHTML;
+  const smile = document.getElementById("smile").innerHTML;
+  const moon = document.getElementById("moon").innerHTML;
+  const sun = document.getElementById("sun").innerHTML;
+  const leaf = document.getElementById("leaf").innerHTML;
+  const tree = document.getElementById("tree").innerHTML;
+  const dove = document.getElementById("dove").innerHTML;
+  const frog = document.getElementById("frog").innerHTML;
+  const house = document.getElementById("house").innerHTML;
+
+  const gameArray = [
+    zero,
+    one,
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven,
+    eight,
+    nine,
+    a,
+    b,
+    c,
+    d,
+    e,
+    f,
+    g,
+    h,
+    i,
+    j,
+    star,
+    heart,
+    smile,
+    moon,
+    sun,
+    leaf,
+    tree,
+    dove,
+    frog,
+    house,
+  ];
+  console.log(gameArray);
+
+  let randomIndex= Math.floor(Math.random() * gameArray.length);
+  console.log(randomIndex);
+
+  let randomGameArrayItem = gameArray[randomIndex];
+  console.log(randomGameArrayItem);
