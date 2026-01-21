86dcb58752877d992ff5511a06740e1d60e17c29
Analisa1984 <ana_lisa8@hotmail.com>
2026-01-21 23:15:25 +0000

    Correct input on buttons.

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -49,7 +49,7 @@ let gameScore = 0;
         fa = gameArray[i];
       }
 
-      html += `<button id="${gameArray[i]}"><i button-name="${gameArray[i]}" class="fa-solid fa-${fa}" aria-label="input ${fa}" data-type="${gameArray[i]}" input-type="button"></i></button>`;
+      html += `<button id="${gameArray[i]}"><i button-name="${gameArray[i]}" class="fa-solid fa-${fa}" aria-label="${fa}" data-type="${gameArray[i]}" ></i></button>`;
 
       //split buttons into rows of 6
 
