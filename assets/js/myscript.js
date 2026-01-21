38db0b7a962dbf9c9ae9036802fb2072b2751a38
Analisa1984 <ana_lisa8@hotmail.com>
2026-01-21 16:58:07 +0000

    Use javascript to create game array buttons.  Add function to display user answer, add function for initializing the randomized cards. Create function to store the randomized cards to be accesssible for the various rounds of the game to be called from. Create  function to convert memory array to string to compare easily. Create start game function. Create showGame round function, gameScore function, new game function and deletecard function. Create function to check user answer which compares user answer to the random card generated.

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,95 +1,262 @@
-let userAnswer; //document.getElementById("user-answer").value;
-let score = 0;
-let buttons = document.getElementsByTagName("button");
-let round = 1;
+let userAnswer = "";
+let maximumRounds = 10;
+let userEntryArray = [];
+const maxMemoryItems = 10;
+let memoryArray = [];
+let gameStage = 0;
+let gameScore = 0;
 
-const zero = document.getElementById("zero").innerHTML;
-  const one = document.getElementById("one").innerHTML;
-  const two = document.getElementById("two").innerHTML;
-  const three = document.getElementById("three").innerHTML;
-  const four = document.getElementById("four").innerHTML;
-  const five = document.getElementById("five").innerHTML;
-  const six = document.getElementById("six").innerHTML;
-  const seven = document.getElementById("seven").innerHTML;
-  const eight = document.getElementById("eight").innerHTML;
-  const nine = document.getElementById("nine").innerHTML;
 
-  const a = document.getElementById("a").innerHTML;
-  const b = document.getElementById("b").innerHTML;
-  const c = document.getElementById("c").innerHTML;
-  const d = document.getElementById("d").innerHTML;
-  const e = document.getElementById("e").innerHTML;
-  const f = document.getElementById("f").innerHTML;
-  const g = document.getElementById("g").innerHTML;
-  const h = document.getElementById("h").innerHTML;
-  const i = document.getElementById("i").innerHTML;
-  const j = document.getElementById("j").innerHTML;
-  const star = document.getElementById("star").innerHTML;
-  const heart = document.getElementById("heart").innerHTML;
-  const smile = document.getElementById("smile").innerHTML;
-  const moon = document.getElementById("moon").innerHTML;
-  const sun = document.getElementById("sun").innerHTML;
-  const leaf = document.getElementById("leaf").innerHTML;
-  const tree = document.getElementById("tree").innerHTML;
-  const dove = document.getElementById("dove").innerHTML;
-  const frog = document.getElementById("frog").innerHTML;
-  const house = document.getElementById("house").innerHTML;
 
   const gameArray = [
-    zero,
-    one,
-    two,
-    three,
-    four,
-    five,
-    six,
-    seven,
-    eight,
-    nine,
-    a,
-    b,
-    c,
-    d,
-    e,
-    f,
-    g,
-    h,
-    i,
-    j,
-    star,
-    heart,
-    smile,
-    moon,
-    sun,
-    leaf,
-    tree,
-    dove,
-    frog,
-    house,
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "star",
+    "heart","smile", "moon", "sun", "leaf", "tree", "dove", "frog", "house"
   ];
   console.log(gameArray);
 
-  let randomIndex= Math.floor(Math.random() * gameArray.length);
-  console.log(randomIndex);
+  document.addEventListener("DOMContentLoaded", function() {
+ 
+  buildButtons();
 
-  let randomGameArrayItem = gameArray[randomIndex];
-  console.log(randomGameArrayItem);
+ 
+  let submitButton = document.getElementById("user-entry-final");
+  submitButton.addEventListener ("click", checkAnswer);
 
-  document.addEventListener("DOMContentLoaded", function () {
+  let deleteButton = document.getElementById("delete");
+  deleteButton.addEventListener ("click", deleteCard);
+
+  let startGameButton = document.getElementById("start-game");
+  startGameButton.addEventListener ("click", startGame);
+
+  let newGameButton = document.getElementById("new-game");
+  newGameButton.addEventListener ("click", newGame);
+
+  let fontawesomeButtons = document.getElementsByClassName("fa-solid");
+  for(let i=0; i <fontawesomeButtons.length; i++ ) {
+       fontawesomeButtons[i].addEventListener ("click", addToUserAnswer);
+    }
+});
+
+    function buildButtons () {
+      let html = "<div>";
+      let fa = ""; 
+
+      for (let i = 0; i < gameArray.length; i++) {
+       if (i <= 9) { 
+        fa = i; 
+      } else {
+        fa = gameArray[i];
+      }
+
+      html += `<button id="${gameArray[i]}"><i button-name="${gameArray[i]}" class="fa-solid fa-${fa}" data-type="${gameArray[i]}"></i></button>`;
+
+      //split buttons into rows of 6
+
+      if ((i + 1) > 1 && (i + 1) % 6 == 0) {
+        html += "</div>";
+        if (i < gameArray.length) {
+          html += "<div>";
+        }
+      }
+
+    }
+
+      // write icons into the div
+      let fontAwesome = document.getElementById("fontAwesome");
+      fontAwesome.innerHTML = html;
+      }
+
+      /**
+       * Randomly generate a numebr (maxMemoryItems) of items to remember 
+       */
+    function initialMemoryItems() {
+      // empty the array first
+      memoryArray = [];
+      //fill array with 3 random chosen items from the gameArray
+      for (let i = 0; i < maxMemoryItems; i++) {
+        memoryArray.push(gameArray[Math.floor(Math.random() * gameArray.length)]);
+      }
+    }
+
+  /**
+   * create a comma-separated list of items to be remembered for the current game stage
+   * returns string e.g. "five, hear, b"
+   */
+    function toStringSubset() {
+      let memoryString = "";
+      for(let i = 0; i < gameStage; i++) {
+        if(i > 0) {
+          memoryString += ",";
+        }
+        memoryString += memoryArray[i];
+      }
+
+      return memoryString;
+    }
+ 
+    
+  function startGame() {
+    initialMemoryItems();
+    userEntryArray = [];
+    gameStage = 1;
+    gameScore = 0;
+    showGameRound();
+    showGameScore();
+    showMemoryChallenge();
+    showUserEntry(true);
+
+    //disable the start game button
+    document.getElementById("start-game").disabled = true;
+    }
+
+    /**
+     * start a new game
+     */
+  function newGame() {
+    if (confirm("Are you ready for a new game?")) {
+      startGame();
+
+    }
+  }
+
+  /**
+   * update the score on the screen
+   */
+  function showGameScore() {
+    document.getElementById("score-board").textContent = `Score: ${gameScore}`;
+  }
+
+  /**
+   * update the game round shown on screen 
+   */
+
+  function showGameRound() {
+    document. getElementById("game-round"). textContent = `Round: ${gameStage}`;
+  }
+
+/**
+ * show the user the random items to be remembered
+ */
+  function showMemoryChallenge() {
+ //create the buttons
+  let btn;
+  let btnHTML = "";
+  for (let i = 0; i < gameStage ; i++) {
+    btn = document.getElementById(memoryArray[i]);
+    btnHTML += `<button>${btn.innerHTML}</button>`;
+  }
+
+  //get location of where to display the random card item(s) and display it/them
+  let randomItemsDisplay = document.getElementById("random-items-display");
+  randomItemsDisplay.innerHTML = `memorize this ${btnHTML}`;
+
+
+  //set timer to display random card items for 3 seconds
+     setTimeout(function () {
+    randomItemsDisplay.innerHTML = " "; 
+  }, 3000);
+  }  
   
 
-  for (let button of buttons) {
-    button.addEventListener("click", function () {
-      if (this.getAttribute("data-type") === "start-game") {
-        console.log("im at line 81 after start game");
-
-
-        startGame();
-        displayUserEntry();
-        document.getElementById("start-game").disabled = true;
-      } else {
-        //checkAnswer();
-      }
-    });
+  /**
+   * display user's answer choice
+   */
+  function showUserEntry() { console.log("Hello World");
+    let btnHTML = "";
+    let btn = "";
+    for (let i=0; i < userEntryArray.length; i++) {
+    btn = document.getElementById(userEntryArray[i]);
+    btnHTML += `<button>${btn.innerHTML}</button>`;
+    }
+    document.getElementById('user-reply').innerHTML = btnHTML;
+   
   }
-});
+
+
+
+  /**
+   * add delete function so user can remove their selection of card 
+   * e.g. if wrong button was clicked by user or they want to change their choice of card.
+   */
+
+   function deleteCard() {
+    userEntryArray.pop();
+    showUserEntry();
+  }
+
+  /**
+   * when user clicks on a button, add it to the user's answer 
+   * (make the answer string allowed by user input a maximum of 3)
+   * update the display of user answers
+   */
+
+   function addToUserAnswer() {console.log("pushing");
+    if (userEntryArray.length < maxMemoryItems) {
+      userEntryArray.push(this.getAttribute("data-type")); 
+      showUserEntry();
+    }
+   }
+  
+
+   /**
+    * check to see if the user has enetered the correct random card shown
+    */
+  function checkAnswer() {
+   if (gameStage == 0) {
+    alert("please start a new game first");
+
+   } else if (gameStage != userEntryArray.length) {
+    alert("That's not the right number of items");
+
+   } else {
+       if (userEntryArray.toString() == toStringSubset()) {
+        //check for game over
+        if (isGameOver()) {
+          gameScore += 10;
+          showGameScore();
+          alert(`You remembered correctly! Your score is now ${gameScore}`)
+          endGame();
+        } else {
+          //game not over yet, update the score and add on a next random cad to be remembered
+          alert("Well done! On to the next round!")
+          userEntryArray = [];
+          gameStage++;
+          gameScore += 10;
+          showGameRound();
+          showGameScore();
+          showUserEntry();
+          showMemoryChallenge();
+        }
+       } else {
+        alert("Oops! Thats Incorrect!")
+        // 2 point reduction in points for incorrect answer choice. 
+        gameScore -= 2;
+        showGameScore();
+       }
+      }
+  }
+ 
+
+  /**
+   * check to see if game is over (has 3 rounds been completed.)
+   */
+ function isGameOver() {
+   return (gameStage >= maxMemoryItems);
+ }
+
+ /**
+  * game is over so ask if the user wants a new game. 
+  */
+ function endGame() {
+  if (confirm("Would you like a new game?")) {
+    startGame();
+  } else {
+    //disable submit button if no new game is selected. 
+    alert("We hope you enjoyed playing Memory Monkey!, Come back again soon!")
+  let submit = document.getElementById("user-entry-final");
+  submit.disabled = true; 
+  }
+  
+ }
+  
